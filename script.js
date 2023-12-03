e0fc3762e504a7ce467a5eb0779cf851fcc750a6
GregSom-MSc <145716530+GregSom-MSc@users.noreply.github.com>
2023-12-03 19:58:05 +0000

    java modified

--- script.js
+++ script.js
@@ -2,18 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('nav a');
 
     navLinks.forEach(link => {
-        link.addEventListener('click', smoothScroll);
     });
 
-    function smoothScroll(e) {
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-
-        window.scrollTo({
-            top: targetSection.offsetTop,
-            behavior: 'smooth'
-        });
-    }
 });
